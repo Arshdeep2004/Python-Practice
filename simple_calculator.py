cf10fcf0f4a50faf8e2e7608a79664c27314ee71
Arshdeep <arshdeep252004@gmail.com>
2026-04-13 19:36:17 +0530

    Simple calulator performing addition,substraction,mul,div

---
+++ simple_calculator.py
@@ -0,0 +1,33 @@
+#Take 2 numbers form the User and perform:
+#1. ADDITON
+#2. SUBSTRACTION
+#3. MULTIPLICATION
+#4. DIVISION
+# num1=int(input("Enter num1 :"))
+# num2=int(input("Enter num2 :"))
+# while(True):
+#     print("1. ADDITON")
+#     print("2. SUBSTRACTION")
+#     print("3. MULTIPLICATION")
+#     print("4. DIVISION")
+#     choice=int(input("Enter your Choice"))
+#     match(choice):
+#         case 1:
+#             sum=num1+num2
+#             print("The sum is :",sum)
+#         case 2:
+#             diff=num1-num2
+#             print("Ther difference is :",diff)
+#         case 3:
+#             prod=num1*num2
+#             print("The product is :",prod)
+#         case 4:
+#             div=num1/num2
+#             print("The Quotient is :",div)
+#         case _:
+#             print("Enter correct choice!") 
+#             break
+                  
+        
+        
+        

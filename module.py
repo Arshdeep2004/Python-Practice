64b24c0aa03eb5807e5ff00e2f9bfdc26e4134f8
Arshdeep <arshdeep252004@gmail.com>
2026-04-27 23:11:36 +0530

    modules

---
+++ module.py
@@ -0,0 +1,36 @@
+#A module is simply a file continuing python code
+#1. Built in module- math,randon,calender,time
+#2.External module- need installation (pip install)
+#3. user defined modules- for eg our own files module.py,first.py,second.py
+
+# import time
+# while True:
+#     print("Hello")
+#     time.sleep(2)
+#     print("Hii")
+#     time.sleep(1)
+
+
+# import phonenumbers
+# from phonenumbers import geocoder
+# a=phonenumbers.parse("+911415161578")
+# print(geocoder.description_for_number(a,"en"))
+
+# import phonenumbers                                                          
+# number = phonenumbers.parse("+919876543210")
+# print(number)
+
+# import calendar
+ # print(calendar.calendar(2004))
+# print(calendar.month(2004,4))
+
+# import math
+# print(math.sqrt(49))
+# print(math.pi)
+# print(math.factorial(5))
+# print(math.ceil(math.sqrt(36)))
+
+# import random
+# print(random.random())
+# print(random.randint(1,100))
+# print(random.randrange(1,10))

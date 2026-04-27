2bc2326aa236f7f38ce4f2b6c68e6f0160005123
Arshdeep <arshdeep252004@gmail.com>
2026-04-27 23:13:13 +0530

    tupples introduction

---
+++ tuples.py
@@ -0,0 +1,54 @@
+#you define tuple with parentheses()
+# a=(10,8.3,"lakshya",True)
+# print(a)
+#you can also define tuple without parentheses
+# a=10,8.3,"lakshya",True
+# print(a)
+# print(a[0])
+# print(a[2])
+# print(a[-1])
+
+# a[0]=80   #this will give error
+# print(a)
+# a.append(85) # this will also give error
+# print(a)
+#packaging and unpackaging 
+# a="lakshya"
+# b="institution"
+# tuple1=()
+# tuple1=a,b
+# print(tuple1)
+# tuple1=('lakshya','institute')
+# x,y=tuple1
+# print(x)
+# print(y)
+
+# tuple2=('lakshya','institute')
+# print(tuple2[0][3])
+# tuple3=('lakshya','Institute')
+# print(tuple3[1][3])
+
+# tuple3=("lakshya institute")
+# print(tuple3[0:13])
+# print(tuple3[0:13:2])
+
+# tuple4=("lakshya",)*10
+# print(tuple4)
+
+# a=(10,2.3,"lakshya",True)
+# print(a)
+# for i in a:
+#     print(i)
+
+# a[0]=80
+# print(a)
+# a.append(84)#immutable cannot be changed
+# print(a)
+
+# tuple5=(12,30,85,74)
+# a=list(tuple5)
+# a.append(85)
+# print(a)
+
+# b=tuple(a)
+# print(b)

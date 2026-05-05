f8aaa74500b61663da63f08a587c00414fdb283e
Arshdeep <arshdeep252004@gmail.com>
2026-05-05 21:10:42 +0530

    fibonacci series question

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+#fibonacci series question-------------------------------------
+
+# n=int(input("Enter till no. you want fibonacci series:"))
+# a=0
+# b=1
+# if (n<0):
+#     print("please enter a positive no.")
+# elif(n==1):
+#     print("Fibonacci sequence:")
+#     print(b)
+# else:
+#     print("Fibonacci sequence:")
+#     while a<=n:
+#         print(a, end=" ")
+#         a,b=b,a+b
+
